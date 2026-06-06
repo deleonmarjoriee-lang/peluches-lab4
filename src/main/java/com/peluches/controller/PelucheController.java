ec0b8c806b537cfb05d7f4c2bd845e9efc80db40
Tamara Castillo <deleonmarjoriee@gmail.com>
2026-06-06 10:43:34 -0600

    Mejora visual

--- src/main/java/com/peluches/controller/PelucheController.java
+++ src/main/java/com/peluches/controller/PelucheController.java
@@ -3,22 +3,61 @@ package com.peluches.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+import com.peluches.models.Peluche;
+import com.peluches.service.ICategoriaService;
 import com.peluches.service.IPelucheService;
 
 @Controller
+@RequestMapping("/peluches")
 public class PelucheController {
 
     @Autowired
     private IPelucheService service;
 
-    @GetMapping("/peluches")
+    @Autowired
+    private ICategoriaService categoriaService;
+
+    @GetMapping
     public String listar(Model model) {
 
-        model.addAttribute("peluches",
-                service.listar());
+        model.addAttribute("peluches", service.listar());
 
         return "listarPeluches";
+    }
+
+    @GetMapping("/nuevo")
+    public String nuevo(Model model) {
+
+        model.addAttribute("peluche", new Peluche());
+        model.addAttribute("categorias", categoriaService.listar());
+
+        return "formularioPeluche";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(@ModelAttribute Peluche peluche) {
+
+        service.guardar(peluche);
+
+        return "redirect:/peluches";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editar(@PathVariable Long id, Model model) {
+
+        model.addAttribute("peluche", service.buscar(id));
+        model.addAttribute("categorias", categoriaService.listar());
+
+        return "formularioPeluche";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable Long id) {
+
+        service.eliminar(id);
+
+        return "redirect:/peluches";
     }
 }
